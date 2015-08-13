a00400fe2b8ff85932ddb46e4453d1bbeac94561
Rey David <rdavid@clearcosthealth.com>
2015-08-13 11:36:33 -0700

    initial

---
+++ config.js
@@ -0,0 +1,13 @@
+cchPersonalizedCommunicationOverrides = {
+    contentfulDataSpaceId: 'hz88qspfcalu',
+    contentfulDataSpaceAccessToken: '943cedca8e7523b15879af6c9c0df5004b8db66a0170be4e5badc302be3733a0',
+    contentfulDataSpacePreviewToken: 'c3d0d720a0501ff3d47be33655cb3ce5e6b8614af2d9263993987af3e9ff5f40',
+    hiddenTabs: ["activity"],
+    planInfoEntries: ['medical', 'rx', 'vision', 'dental'],
+    employerId: 11,
+    CCHEndpoint: 'https://lawapi.clearcosthealth.com/v1/Animation',
+    planInfoTabName: {
+        'en': 'ID Cards',
+        'es': 'Tarjetas'
+    }
+};
